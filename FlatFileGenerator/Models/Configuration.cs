5d9a10d859d883b2d2e3a50f2093bbe7d9ea1cb5
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2020-01-07 18:42:06 +0530

    Save commit

---
+++ FlatFileGenerator/Models/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatFileGenerator.Models
+{
+    internal class Configuration
+    {
+        public int Rows { get; set; }
+        public List<Columns> Columns { get; set; }
+
+        public static async Task<Configuration> GetCurrentConfiguration()
+        {
+            var fileName = "config.json";
+            var fileNameWithPath = Path.Join(Directory.GetCurrentDirectory(), fileName);
+            var configJson = await File.ReadAllTextAsync(fileNameWithPath);
+            return JsonConvert.DeserializeObject<Configuration>(configJson);
+        }
+    }
+
+    internal class Columns
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ FlatFileGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlatFileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
