c97392a6d753eeba28bbb0d56958b1588b55bf75
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2020-01-09 22:29:00 +0530

    Dictionary Extension methods

---
+++ FlatFileGenerator/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlatFileGenerator.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static T GetValueAndCompare<T>(this Dictionary<string, string> dict, string key, object expectedValue)
+        {
+            T value;
+            string dictValue;
+            dict.TryGetValue(key, out dictValue);
+            if(string.IsNullOrEmpty(dictValue))
+            {
+                value = (T)expectedValue;
+            }
+            else
+            {
+                value = (T)Convert.ChangeType(dictValue, typeof(T));
+            }
+
+            return value;
+        }
+    }
+}
